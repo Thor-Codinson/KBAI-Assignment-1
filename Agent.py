001f45e03dcc411621cd791d88bdd6168b767978
Kyle <kyledexsmith@gmail.com>
2018-02-08 19:09:24 -0600

    Cleaned up
    First submission

--- Agent.py
+++ Agent.py
@@ -1,9 +1,9 @@
-from PIL import Image, ImageChops as Chops, ImageOps as Ops
+from PIL import Image, ImageChops as Chops, ImageOps as Ops, ImageFilter as Filter
 import Transformer
 import numpy as np
 import os
 import sys
-
+import time
 
 ImgSize = (180,180)
 ci= 98
@@ -12,66 +12,46 @@ class Agent:
 
     def __init__(self):
         self.root_dir = sys.path[0]
+        self.time = time.clock()
 
 
     def Solve(self, problem):
         print("Trying to solve", problem.name, "// Type is: ", problem.problemType, " // Visual: ", problem.hasVisual,
               " // Verbal: ", problem.hasVerbal)
+        self.time = time.time()
         guess = -1
         transA = []
         transNum = None
         answerList = []
-        fillList = []
-        if problem.problemType == "2x2":
-            try:
-                imgA = self.load(problem, 'A')
-                imgB = self.load(problem, 'B')
-                imgC = self.load(problem, 'C')
-                transA = self.runTrans(imgA, transA)
-                transNum = self.compare(imgB, transA)
-                if transNum == 17:
-                    transNum = self.compare(imgC, transA)
-                    answerImg = self.newTrans(transNum, imgB)
-                else:
-                    answerImg = self.newTrans(transNum, imgC)
-                img1 = self.load(problem, '1')
-                img2 = self.load(problem, '2')
-                img3 = self.load(problem, '3')
-                img4 = self.load(problem, '4')
-                img5 = self.load(problem, '5')
-                img6 = self.load(problem, '6')
+        try:
+            imgA = self.load(problem, 'A')
+            imgB = self.load(problem, 'B')
+            imgC = self.load(problem, 'C')
+            transA = self.runTrans(imgA, transA)
+            transNum = self.compare(imgB, transA)
+            if transNum == -1:
+                transNum = self.compare(imgC, transA)
+                answerImg = self.newTrans(transNum, imgB)
+            else:
+                answerImg = self.newTrans(transNum, imgC)
+            img1 = self.load(problem, '1')
+            img2 = self.load(problem, '2')
+            img3 = self.load(problem, '3')
+            img4 = self.load(problem, '4')
+            img5 = self.load(problem, '5')
+            img6 = self.load(problem, '6')
 
-                answerList.append(img1)
-                answerList.append(img2)
-                answerList.append(img3)
-                answerList.append(img4)
-                answerList.append(img5)
-                answerList.append(img6)
-                if transNum == 17:
-                    print("Creating fill list")
-                    fillList.append(Transformer.answerFill(imgC, img1))
-                    fillList.append(Transformer.answerFill(imgC, img2))
-                    fillList.append(Transformer.answerFill(imgC, img3))
-                    imgTest3 = Transformer.answerFill(imgC, img3)
-                    imgTest3.save("_IMAGE3.png")
-                    fillList.append(Transformer.answerFill(imgC, img4))
-                    fillList.append(Transformer.answerFill(imgC, img5))
-                    imgTest5 = Transformer.answerFill(imgC, img5)
-                    imgTest5.save("_IMAGE5.png")
-                    fillList.append(Transformer.answerFill(imgC, img6))
-                    imgTest1 = Transformer.answerFill(imgC, img1)
-                    imgTest1.save("_IMAGE1.png")
-
-                if transNum == 17:
-                    print("trying a fill guess")
-                    guess = self.compare(imgC, fillList) + 1
-                else:
-                    print("trying a normal guess")
-                    guess = self.compare(answerImg, answerList) + 1
-                print("Final answer is: ", guess)
-            except Exception as e:
-                print(e)
-
+            answerList.append(img1)
+            answerList.append(img2)
+            answerList.append(img3)
+            answerList.append(img4)
+            answerList.append(img5)
+            answerList.append(img6)
+            guess = self.compare(answerImg, answerList) + 1
+            print("Final answer is: ", guess)
+        except Exception as e:
+            print(e)
+        self.printTime()
         return guess
 
 
@@ -94,16 +74,18 @@ class Agent:
         transList.append(Transformer.rotate(img, 270))
         transList.append(Transformer.rotate(img, 315))
         return transList
+
     def compare(self, img, transList):
         score = 0
         for image in transList:
            score = Transformer.getDif(img, image)
            if score > ci:
                print("match found")
-               image.save("_ANSWERIMAGE.png")
+               # image.save("_ANSWERIMAGE.png")
                return transList.index(image)
         print("Nothing matched")
-        return 17
+        return -1
+
     def newTrans(self, transNum, image):
         if transNum == 0:
             return image
@@ -126,18 +108,8 @@ class Agent:
         if transNum == 9:
             return Transformer.rotate(image, 315)
 
-    def fillCompare(self, imgA, imgB, imgC):
-        aFill = Transformer.fill(imgA, imgB)
-        scoreB = Transformer.getDif(aFill, imgB)
-        scoreC = Transformer.getDif(aFill, imgC)
-        if scoreB > ci:
-            print("B Fill found")
-            return aFill
-        if scoreC > ci:
-            print("C Fill found")
-            return aFill
-
-
-
-
-
+    def printTime(self):
+        elapsed = time.time() - self.time
+        print()
+        print('Took', int(elapsed * 1000), 'ms')
+        self.time = time.time()
--- Transformer.py
+++ Transformer.py
@@ -1,4 +1,4 @@
-from PIL import Image, ImageChops
+from PIL import Image, ImageChops, ImageFilter
 import numpy
 import os
 import sys
@@ -6,30 +6,40 @@ import sys
 
 ci = 95
 
+
 def reflect_horz(image):
     return image.transpose(Image.FLIP_LEFT_RIGHT)
+
 
 def reflect_vert(image):
     return image.transpose(Image.FLIP_TOP_BOTTOM)
 
+
 def rotate(image, degrees):
     return image.rotate(degrees, 0, 0)
+
 
 def subtract(image1, image2):
     return ImageChops.subtract(image1, image2)
 
+
 def add(image1, image2):
     return ImageChops.add(image1, image2)
+
 
 def fill(image1, image2):
     return ImageChops.darker(image1,image2)
 
-def answerFill(image1, image2):
-    # image1 = ImageChops.invert(image1)
-    image3 = ImageChops.screen(image1, image2)
-    # image3 = ImageChops.invert(image3)
 
-    return image3
+def answerFill(image1):
+    # image1 = ImageChops.invert(image1)
+    # image1= image1.filter(ImageFilter.FIND_EDGES)
+    # image1 =ImageChops.invert(image1)
+    # image1 = ImageChops.invert(image1)
+    # image3 = ImageChops.darker(image1, image2)
+    # image3 = ImageChops.invert(image3)
+    return image1
+
 
 def getDif(image1, image2):
     diff = ImageChops.difference(image1, image2)
@@ -39,6 +49,7 @@ def getDif(image1, image2):
     # diff.save(os.path.join(sys.path[0], '_diff.png'))
     total = diff.size[0] * diff.size[1]
     return(numpy.count_nonzero(diff) / total) * 100
+
 
 def match(image1, image2):
     matchValue = getDif(image1, image2)
